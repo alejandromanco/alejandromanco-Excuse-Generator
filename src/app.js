496331a6f679d6acde76fa629da55a396df2cae0
alejandromanco <92924749+alejandromanco@users.noreply.github.com>
2023-12-05 17:11:56 +0000

    excuse-generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["figther", "raccon", "dog", "driver", "comedian", "monster"];
+  let action = ["took my", "threw my", "stole my", "bit my", "punched my"];
+  let possetion = ["homework", "toe", "car", "shoe", "cellphone"];
+  let where = ["on the street", "in my house", "in my driveway"];
+
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subIndex = Math.floor(Math.random() * subject.length);
+  let actIndex = Math.floor(Math.random() * action.length);
+  let posIndex = Math.floor(Math.random() * possetion.length);
+  let wheIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndex] +
+    "" +
+    subject[subIndex] +
+    "" +
+    action[actIndex] +
+    "" +
+    possetion[posIndex] +
+    "" +
+    where[wheIndex]
+  );
 };
